5a38dc99855c050735f8de4384fd44c54aed0f41
Roy-Mora <153629093+Roy-Mora@users.noreply.github.com>
2023-12-12 09:25:44 -0500

    Add files via upload

---
+++ Programa.py
@@ -0,0 +1,42 @@
+#Ejercicio 1
+#Suma de 2 numeros enteros
+a = 2
+b = 8
+c = a + b
+print (c) #imprimir c
+#Resta de 2 numeros de punto flotante
+d = 15.7
+e = 5.5
+f = d - e
+print (f) #imprimir f
+#Multiplicacion de 2 numeros enteros
+g = 9
+h = 8
+i = g * h
+print (i) #imprimir i
+#Division de 2 numeros de punto flotante
+j = 64.8
+k = 8.2
+l = j / k
+print (l) #imprimir l
+
+#Ejercicio 2
+#Variable String
+#el proposito de esta variable es saber cuantos numeros de letras hay entre todas las palabras que esctibimos
+m = "Royert Renee Mora Heras" #Este es mi nombre completo escrito
+longitud_nombre = len(m) #el proposito de este es mostrarnos la longitud en cadena
+print(longitud_nombre) # Esto mostrara la longitud de tu nombre en caracteres
+
+#Ejercicio 3
+#Conversion de tipos de datos
+n = 8 #este es entera
+o = 12.3 #esta es flotante
+print("\nDato entero :",type(n)) #impresion del tipo del dato
+print("Dato flotante :",type(o)) #impresion del tipo de dato 2
+entero = float(n) #cambiamos de entero a flotante
+flotante = int(o) #cambiamos de flotante a entero
+
+print("+++++++++++++++++++++++++++++++++++++")
+
+print("Dato entero : ", type(entero)) #invertir el tipo de dato
+print("Dato flotante : ", type(flotante)) #invertir de nuevo
